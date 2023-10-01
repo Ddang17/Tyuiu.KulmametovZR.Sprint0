ef6db328432338b1c692b665cbb61eca7f00d0a6
tyuiu.kulmametovzr@outlook.com <kulmametovzr.tyuiu@gmail.com>
2023-10-01 15:53:18 +0500

    Task2

---
+++ Tyuiu.KulmametovZR.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KulmametovZR.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.KulmametovZR.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Заур";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Заур", res);
+        }
+    }
+}
