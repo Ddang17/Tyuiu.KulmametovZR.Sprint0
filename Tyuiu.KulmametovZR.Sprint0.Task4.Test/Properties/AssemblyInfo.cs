f8dd99ccd8b9edf5fe00f18321cabcc34b5412e7
tyuiu.kulmametovzr@outlook.com <kulmametovzr.tyuiu@gmail.com>
2023-10-01 16:59:08 +0500

    Task4

---
+++ Tyuiu.KulmametovZR.Sprint0.Task4.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KulmametovZR.Sprint0.Task4.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KulmametovZR.Sprint0.Task4.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e6c649db-0107-4e05-9fa5-ebf2ef6e91b4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
